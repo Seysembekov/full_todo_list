c5a84a8e623451eb76f6e2e3253da626ca3c718b
Islam Seisembekov <soulsdestroyer1902@gmail.com>
2026-06-06 17:50:01 +0500

    created dependencies in api

--- todo-app-backend/src/api/dependencies.py
+++ todo-app-backend/src/api/dependencies.py
@@ -1,3 +1,11 @@
 from sqlalchemy.orm import Session
 from database import get_db
-from redis_clie
+from redis_client import get_redis
+from fastapi import Depends
+from redis.asyncio import Redis
+
+def get_db_Session(db: Session = Depends(get_db)):
+    return db
+
+async def get_redis_client(redis: Redis = Depends(get_redis)):
+    return redis
