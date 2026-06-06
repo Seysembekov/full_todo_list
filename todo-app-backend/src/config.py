c2910610f55f949274092c1bf03ecb02f4dd1cf9
Islam Seisembekov <soulsdestroyer1902@gmail.com>
2026-06-06 17:36:06 +0500

    add redis parameters into config

--- todo-app-backend/src/config.py
+++ todo-app-backend/src/config.py
@@ -4,6 +4,10 @@ class Settings(BaseSettings):
     DATABASE_NAME: str = "db"
     DATABASE_URL: str = "postgresql+psycopg://postgres:admin@127.0.0.1:5432/db"
 
+    REDIS_HOST: str = 'localhost'
+    REDIS_PORT: str = 6379
+    REDIS_DB: int = 0
+    CACHE_EXPIRE: int = 300
 
     class Config:
         env_file = '.env'
